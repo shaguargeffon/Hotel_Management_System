90fdb9a0c4e6015d7ea9a206af5fe74c3775bfb9
shaguargeffon <shaguarhan123@gmail.com>
2022-10-03 00:08:36 -0700

    Add-server-basic_implementation

---
+++ server/server.cpp
@@ -0,0 +1,68 @@
+#include "server.hpp"
+
+
+TcpServer::TcpServer(int port, unsigned int amount_clients): server_port(port), max_clients(amount_clients)
+{
+}
+
+void TcpServer::create_socket() 
+{
+    sfd = socket(AF_INET, SOCK_STREAM, 0); //create server socket
+}
+
+void TcpServer::set_ip_format()
+{
+    serv_addr.sin_family = AF_INET; //IPV4 is used
+}
+
+void TcpServer::set_server_port()
+{
+    serv_addr.sin_port = htons(server_port); //bind the port
+}
+
+void TcpServer::configure_valid_ip_address()
+{
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); //listen to all IP address
+}
+
+void TcpServer::bind_server() 
+{
+    bind(sfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+}
+
+void TcpServer::listen_clients()
+{
+    listen(sfd, max_clients);  //max_clients means how many clients are allowed to connet to the server at the same time
+}
+
+void TcpServer::accpet_clients()
+{
+    clie_len = sizeof(clie_addr);
+    cfd = accept(sfd, (struct sockaddr*)&clie_addr, &clie_len); //blocking mode
+}
+
+void TcpServer::close_server()
+{
+    close(sfd);
+}
+
+void TcpServer::close_client()
+{
+    close(cfd);
+}
+
+
+void TcpServer::start_server()
+{
+    while(1)
+    {
+        int n = read(cfd, buf, sizeof(buf));
+        
+        for(int i = 0; i < n; i++)
+        {   
+            buf[i] = toupper(buf[i]); //read data from client
+        }
+
+	    write(cfd, buf, n);
+    }
+}
---
+++ server/server.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+
+
+class TcpServer
+{
+public:
+
+    TcpServer(int port=1111, unsigned int amount_clients=10);
+
+    void create_socket(); 
+
+    void set_ip_format();
+
+    void set_server_port();
+
+    void configure_valid_ip_address();
+
+    void bind_server();
+
+    void listen_clients();
+
+    void accpet_clients();
+
+    void close_server();
+
+    void close_client();
+
+    void start_server();
+
+private:
+    int sfd{0};   //server socket file descriptor
+    int cfd{0};   //client socket file descriptor
+    int server_port; // server port
+    struct sockaddr_in serv_addr;  //struct of server address
+    struct sockaddr_in clie_addr;  //struct of client address
+    socklen_t clie_len{0}; //size of client struct
+    char buf[BUFSIZ]; //buffer to save message from client
+    unsigned int max_clients; // maximal clients connected at the same time
+};
+
+
