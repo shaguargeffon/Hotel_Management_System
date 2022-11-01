e24ac36d90b864a4d8486a25c47271aaccda3984
shaguargeffon <shaguarhan123@gmail.com>
2022-11-01 16:18:56 +0100

    implementation-protocal-register

--- protocal/protocal.hpp
+++ protocal/protocal.hpp
@@ -22,6 +22,73 @@ public:
 };
 
 
+//Request frame: Frame_ID (2bytes) + Name (10bytes) + Password(7bytes)
+//Response frame: Frame_ID (3bytes) + Client_ID (5bytes) + Register_Status (1byte)
+class ProtocalRegister: public Protocal
+{
+public:
+    virtual bool parse_request_frame()
+    {
+        memory_copy(frame_id_request, 0, request_frame_p, 0, 2);
+
+        memory_copy(user_name, 0, request_frame_p, 2, 10);
+
+        memory_copy(password, 0, request_frame_p, 12, 7);
+
+        return true;
+    }
+
+    //TBD: implemente checking user name from data base.
+    bool is_user_name_registered(char* name)
+    {
+        return true;
+    }
+
+    virtual bool build_response_frame()
+    {
+        if(is_user_name_registered(user_name))
+        {
+            return false;
+        }
+
+        frame_id_response[0] = '1';
+
+        memory_copy(frame_id_response, 1, frame_id_request, 0, 2);
+
+        memory_copy(client_id, 0, "abcde", 0, 5); // TBD: generate a random client ID
+
+
+        memory_copy(register_status, 0, "1", 0, 1);
+
+
+        //copy the frames into response buffer
+        memory_copy(resonse_buff, 0, frame_id_response, 0, 3);
+        memory_copy(resonse_buff, 3, client_id, 0, 5);
+        memory_copy(resonse_buff, 8, register_status, 0, 1);
+
+        resonse_buff[9] = '\0';
+
+        return true;        
+    }
+
+    virtual void modify_database()
+    {
+        
+    }
+
+private:
+    char* request_frame_p;
+    //Database* data_base;
+    char frame_id_request[2];
+    char user_name[10];
+    char password[7];
+    char frame_id_response[3];
+    char client_id[5];
+    char register_status[1];
+    char resonse_buff[10];
+};
+
+
 /*
 class ProtocalCancelBookingRegistered: public Protocal
 {
@@ -119,71 +186,9 @@ private:
 
 
 
-//Request frame: Frame_ID (2bytes) + Name (10bytes) + Password(7bytes)
-//Response frame: Frame_ID (3bytes) + Client_ID (5bytes) + Register_Status (1byte)
-class ProtocalRegister: public Protocal
-{
-public:
-    virtual bool parse_request_frame()
-    {
-        memory_copy(frame_id_request, 0, request_frame_p, 0, 2);
-
-        memory_copy(user_name, 0, request_frame_p, 2, 10);
-
-        memory_copy(password, 0, request_frame_p, 12, 7);
-
-        return true;
-    }
-
-    //TBD: implemente checking user name from data base.
-    bool is_user_name_registered(char* name)
-    {
-        return true;
-    }
-
-    virtual bool build_response_frame()
-    {
-        if(is_user_name_registered(user_name))
-        {
-            return false;
-        }
-
-        frame_id_response[0] = '1';
-
-        memory_copy(frame_id_response, 1, frame_id_request, 0, 2);
-
-        memory_copy(client_id, 0, "abcde", 0, 5); // TBD: generate a random client ID
 
 
-        memory_copy(register_status, 0, "1", 0, 1);
 
-
-        //copy the frames into response buffer
-        memory_copy(resonse_buff, 0, frame_id_response, 0, 3);
-        memory_copy(resonse_buff, 3, client_id, 0, 5);
-        memory_copy(resonse_buff, 8, register_status, 0, 1);
-
-        resonse_buff[9] = '\0';
-
-        return true;        
-    }
-
-    virtual void modify_database()
-    {
-        
-    }
-
-private:
-    char* request_frame_p;
-    //Database* data_base;
-    char frame_id_request[2];
-    char user_name[10];
-    char password[7];
-    char frame_id_response[3];
-    char client_id[5];
-    char register_status[1];
-    char resonse_buff[10];
-};
 
 
 class ProtocalModificationPassword: public Protocal
