113418102f786df8d4806cc4e34d840498c885dd
shaguargeffon <shaguarhan123@gmail.com>
2022-11-03 23:07:41 +0100

    start-with-customer-db

--- database/database.hpp
+++ database/database.hpp
@@ -7,10 +7,40 @@
 
 using namespace std;
 
+
 class DataBase
 {
 
+public:
+    virtual int open_database()=0;
 
+
+};
+
+
+
+
+
+
+
+class CustomerDataBase: public DataBase
+{
+public:
+
+    CustomerDataBase(const char *db_name): db(NULL), database_name(db_name)
+    {
+    }
+
+    int open_database() final
+    {
+        int rc = sqlite3_open(database_name, &db);
+        return rc;
+    }
+
+
+private:
+    sqlite3 *db;
+    const char *database_name;
 
 };
 
